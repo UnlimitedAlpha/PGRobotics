7a7a12ab3d4af1f9df7de0253a5c04ca6dd99862
UnlimitedAlpha <josiah.thieme@yahoo.com>
2016-12-06 15:07:39 -0500

    Adding basic drive functions

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/PottsgroveTeleOp.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/PottsgroveTeleOp.java
@@ -2,6 +2,7 @@ package org.firstinspires.ftc.teamcode;
 
 import com.qualcomm.robotcore.eventloop.opmode.OpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.util.ElapsedTime;
 
 /**
@@ -12,8 +13,8 @@ public class PottsgroveTeleOp extends OpMode{
     /* Declare OpMode members. */
     private ElapsedTime runtime = new ElapsedTime();
 
-    // private DcMotor leftMotor = null;
-    // private DcMotor rightMotor = null;
+    private DcMotor leftMotor = null;
+    private DcMotor rightMotor = null;
 
     /*
      * Code to run ONCE when the driver hits INIT
@@ -26,13 +27,13 @@ public class PottsgroveTeleOp extends OpMode{
          * to 'get' must correspond to the names assigned during the robot configuration
          * step (using the FTC Robot Controller app on the phone).
          */
-        // leftMotor  = hardwareMap.dcMotor.get("left_drive");
-        // rightMotor = hardwareMap.dcMotor.get("right_drive");
+        leftMotor  = hardwareMap.dcMotor.get("left_drive");
+        rightMotor = hardwareMap.dcMotor.get("right_drive");
 
         // eg: Set the drive motor directions:
         // Reverse the motor that runs backwards when connected directly to the battery
         // leftMotor.setDirection(DcMotor.Direction.FORWARD); // Set to REVERSE if using AndyMark motors
-        //  rightMotor.setDirection(DcMotor.Direction.REVERSE);// Set to FORWARD if using AndyMark motors
+        // rightMotor.setDirection(DcMotor.Direction.REVERSE);// Set to FORWARD if using AndyMark motors
         // telemetry.addData("Status", "Initialized");
     }
 
@@ -59,8 +60,8 @@ public class PottsgroveTeleOp extends OpMode{
         telemetry.addData("Status", "Running: " + runtime.toString());
 
         // eg: Run wheels in tank mode (note: The joystick goes negative when pushed forwards)
-        // leftMotor.setPower(-gamepad1.left_stick_y);
-        // rightMotor.setPower(-gamepad1.right_stick_y);
+        leftMotor.setPower(-gamepad1.left_trigger);
+        rightMotor.setPower(-gamepad1.right_trigger);
     }
 
     /*
